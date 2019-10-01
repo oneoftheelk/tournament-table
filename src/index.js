c7b90354ce2aaed68f3f561a9c51dfe7a309abe0
Raman Tamilin <oneoftheelk@gmail.com>
2019-10-01 22:56:22 +0300

    minor fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.scss';
-import App from './App';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
